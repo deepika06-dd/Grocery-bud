0aa73b359a67a3447269d8d7aa6362ca17437459
deepika21 <laxmisipika98@gmail.com>
2026-02-14 12:44:33 +0545

    change the items

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export const groceryItems = [
-  { id: "1", name: "milk", completed: true },
-  { id: "2", name: "bread", completed: true },
+  { id: "1", name: "Potato", completed: true },
+  { id: "2", name: "Tomato", completed: true },
   { id: "3", name: "eggs", completed: false },
-  { id: "4", name: "butter", completed: false },
+  { id: "4", name: "Coffee", completed: false },
 ];
